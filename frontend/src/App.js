b74f85f5ef35c37d2754f7a74e658c17e98ed94c
LuisFern873 <luis.mendez.l@utec.edu.pe>
2023-03-09 12:36:51 -0500

    including bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import About from './components/About';
 import Users from './components/Users';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <div>
+      <Navbar />
+      <div className = "container p-4">
         <Routes>
           <Route path = '/about' element = {<About />}/>
           <Route path = '/' element = {<Users />}/>
